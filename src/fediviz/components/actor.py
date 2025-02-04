e7469fafcbe3ed7b526aad49cdec45d2fce5048f
Ada <github@adapdr.me>
2025-02-04 19:26:42 +0100

    feat: implemented basic actor component

--- src/fediviz/components/actor.py
+++ src/fediviz/components/actor.py
@@ -7,10 +7,57 @@ File contains actor class, used for:
 actor data from the users upload.
 """
 import streamlit as st
+from utils import Uploads
+from pandas import DataFrame
 
 
 class Actor:
     """This class loads, transforms and displays Actor data"""
 
+    contents: dict
+    http_url: str
+    fedi_url: str
+    name: str
+
     def __init__(self):
-        st.text("NotImplemented")
+        self.contents = Uploads.get_file("actor.json")
+        self.get_links()
+
+        self.show()
+
+    def tags_table(self) -> DataFrame:
+        """Retrieve, process and return actor tags"""
+        df_tags = {"Tag": [], "Description": []}
+        for entry in self.contents["attachment"]:
+            df_tags["Tag"].append(entry["name"])
+            df_tags["Description"].append(entry["value"])
+
+        df = DataFrame(df_tags)
+        df.set_index("Tag", inplace=True)
+        return df
+
+    def get_links(self):
+        """Retrieve and process links for actor"""
+        self.http_url = self.contents["url"]
+
+        server, user = self.http_url.split('@')
+        server = server.replace('/', '').split('https:')[1]
+
+        self.fedi_url = f"@{server}@{user}"
+
+    def show(self):
+        """Calls internal data methods and visualizes results"""
+        with st.container():
+            st.header(self.contents["name"], divider=True)
+            left_column, right_column = st.columns(2)
+            with left_column:
+                st.image(Uploads.get_image("avatar.png"))
+                st.link_button(
+                    label=f"Profile: {self.fedi_url}",
+                    url=self.http_url,
+                    help="Click open profile on-instance",
+                    type="primary"
+                )
+            with right_column:
+                st.image(Uploads.get_image("header.jpg"))
+                st.table(self.tags_table())
