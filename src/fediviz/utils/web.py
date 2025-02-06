dbc056b727ec61e13bb82ea872636e342a5e27fd
Ada <github@adapdr.me>
2025-02-06 13:55:22 +0100

    refactor: webutil simplified to 1 request for f/f

--- src/fediviz/utils/web.py
+++ src/fediviz/utils/web.py
@@ -1,17 +1,13 @@
-"""File contains class used for retrieving user data from the web"""
-import json
+"""File contains WebUtil used for retrieving data using web requests"""
 from typing import Tuple
 from logging import getLogger, Logger
-from zipfile import ZipFile
-import streamlit as st
 import requests
-
 
 logger: Logger = getLogger("__name__")
 
 
-class Web:
-    """This class manages user web data from mastodon servers"""
+class WebUtil:
+    """Provides methods for retriving user web data"""
 
     @staticmethod
     def lookup_account(base_url: str, user_name: str) -> Tuple[int, dict]:
@@ -21,7 +17,7 @@ class Web:
         return data.status_code, data.json()
 
     @staticmethod
-    def get_account(base_url: str, user_id: int):
-        url = f"{base_url}/api/v1/accounts/{user_id}"
+    def get_account(base_url: str, user_id: int) -> Tuple[int, dict]:
+        url = f"{base_url}/api/v1/accounts/{user_id}/"
         data = requests.get(url=url)
         return data.status_code, data.json()
