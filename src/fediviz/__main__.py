60bf0f23e24ef4f6385eb455fd5f3364d440428f
Ada <github@adapdr.me>
2025-02-06 16:02:53 +0100

    feat: new views and icons added to router

--- src/fediviz/__main__.py
+++ src/fediviz/__main__.py
@@ -37,11 +37,14 @@ if __name__ == "__main__":
 
     data_views = {
         "App": [
-            st.Page("views/profile.py", title="Profile", url_path="profile")
+            st.Page("views/profile.py", title="Profile", url_path="profile", icon=":material/person:"),
+            st.Page("views/bookmarks.py", title="Bookmarks", url_path="bookmarks", icon=":material/bookmark:"),
+            st.Page("views/likes.py", title="Likes", url_path="likes", icon=":material/thumb_up:"),
+            st.Page("views/posts.py", title="Posts", url_path="posts", icon=":material/docs:"),
         ],
         "Resources": [
-            st.Page("views/license.py", title="License", url_path="license"),
-            st.Page("views/privacy.py", title="Privacy", url_path="privacy")
+            st.Page("views/license.py", title="License", url_path="license", icon=":material/license:"),
+            st.Page("views/privacy.py", title="Privacy", url_path="privacy", icon=":material/visibility_off:"),
         ]
     }
 
