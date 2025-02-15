5d28de5f04643899b5303b875ee04e17c85b4c55
Ada <github@adapdr.me>
2025-02-15 15:50:57 +0100

    feat: show_header is now a standalone component

--- src/fediviz/components/__init__.py
+++ src/fediviz/components/__init__.py
@@ -1,8 +1,9 @@
 """Module contains a collection of reuseable components"""
 
 from .footer import show_footer
+from .header import show_header
 from .hero import show_hero
 from .license import show_license
 from .privacy import show_privacy
 
-__all__ = ["show_footer", "show_hero", "show_license", "show_privacy"]
+__all__ = ["show_footer", "show_hero", "show_license", "show_privacy", "show_header"]
---
+++ src/fediviz/components/header.py
@@ -0,0 +1,15 @@
+"""File contains reuseable header component"""
+
+import streamlit as st
+from streamlit_extras.stylable_container import stylable_container
+from utils import StorageMode, StorageUtil
+
+
+def show_header(key: str = "header-container"):
+    """Reuseable header component"""
+    css = "{overflow: hidden; max-height: 15vh;}"
+    with stylable_container(key, css):
+        try:
+            st.image(StorageUtil.get_image("header", mode=StorageMode.state))
+        except FileNotFoundError:
+            pass
