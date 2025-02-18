1d476d924c22e3759923ed1113035897e9b845cc
Ada <github@adapdr.me>
2025-02-18 16:21:38 +0100

    style: components now have a background

--- src/fediviz/components/license.py
+++ src/fediviz/components/license.py
@@ -2,7 +2,20 @@
 
 import streamlit as st
 
+css_component = """
+    <style>
+        div[data-testid="stMarkdown"] {
+            background-color: #292938;
+            padding: 20px;
+            border-radius: 20px;
+        }
+    </style>
+"""
+
 
 def show_license():
     """Reuseable license component"""
-    st.markdown(st.session_state["files.LICENSE.md"])
+
+    st.markdown(
+        css_component + st.session_state["files.LICENSE.md"], unsafe_allow_html=True
+    )
