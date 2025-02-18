725a2de0eab265f10db5c023800edeee87c535bf
Ada <github@adapdr.me>
2025-02-18 16:22:27 +0100

    refactor: banner now follows component css structure

--- src/fediviz/components/banner.py
+++ src/fediviz/components/banner.py
@@ -4,10 +4,18 @@ import streamlit as st
 from streamlit_extras.stylable_container import stylable_container
 from utils import StorageMode, StorageUtil
 
+css = """
+    [data-testid="stImageContainer"] {
+        overflow: hidden;
+        max-height: 15vh;
+        border-radius: 20px;
+    }
+"""
+
 
 def show_banner(key: str = "header-container"):
     """Reuseable header component"""
-    css = "{overflow: hidden; max-height: 15vh; border-radius: 20px;}"
+
     with stylable_container(key, css):
         try:  # ? To get the banner
             st.image(StorageUtil.get_image("header", mode=StorageMode.state))
