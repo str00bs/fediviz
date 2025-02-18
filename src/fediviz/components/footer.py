2804918f13c1b718659f30ebd3de141faa363491
Ada <github@adapdr.me>
2025-02-18 16:22:06 +0100

    style: footer now center, removes some global styling

--- src/fediviz/components/footer.py
+++ src/fediviz/components/footer.py
@@ -1,12 +1,24 @@
 """File contains reuseable footer component"""
 
+import streamlit as st
 from streamlit_extras.bottom_container import bottom
 from streamlit_extras.mention import mention
+
+css = """
+    <style>
+        div[data-testid="stBottom"] .stMarkdown {
+            background-color: transparent;
+            padding: 0;
+            text-align: center;
+        }
+    </style>
+"""
 
 
 def show_footer():
     """Reuseable footer component"""
     with bottom():
+        st.markdown(css, True)  # ? Removes padding added by above components
         mention(
             label="github.com/str00bs/fediviz",
             icon="github",
